b48abb7bd2f28865e39bee4095b7e0f66f0b0d87
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-03-19 19:35:41 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240319/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
+++ 240319/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     double a, b, c;
     cin >> a >> b >> c;
+    int tmp;
+    if(a<c){
+        tmp = a;
+        a = c;
+        c = tmp;
+    }    
+    if(c<b){
+        tmp = c;
+        c =b;
+        b = tmp;
+    }
+    if(a < b){
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
 
     int tmpc = (int)(c*10);
     if(tmpc%10 >= 5) tmpc = tmpc/10+1;
