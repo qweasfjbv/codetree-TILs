5bd09fe116ed3798d2be91bcc7f2ad65f347bccc
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-04-07 14:09:07 +0900

    Title: 가장 많은 데이터, Time: 1025ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240407/가장 많은 데이터/most-frequent-data.cpp
+++ 240407/가장 많은 데이터/most-frequent-data.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main() {
     
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
     unordered_map<string, int> map;
 
     int n; string s;
@@ -13,7 +16,7 @@ int m = -1;
     cin >> n;
     for(int i=0; i<n; i++){
         cin >> s;
-        if(map.find(s) == map.end())
+        if(map.count(s) == 0)
             map[s] = 1;
         else map[s] = map[s] + 1;
 
