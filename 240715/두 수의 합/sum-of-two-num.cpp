063dd0d5ce636a6395772d919da58bfa504c2e28
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-15 03:09:40 +0900

    Title: 두 수의 합, Time: 1092ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240715/두 수의 합/sum-of-two-num.cpp
+++ 240715/두 수의 합/sum-of-two-num.cpp
@@ -10,19 +10,22 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
 
+
     // Input
     cin >> n >> k;
     for(int i=0; i<n; i++){
         cin >> n;
-        if(_map.find(n) != _map.end()) _map[n]++;
+        if(_map[n] != 0) _map[n]++;
         else _map[n] = 1;
+
+        
     }
 
     int sum = 0;
     
 
     for(auto elem : _map){
-        if(_map.find(k-elem.first) != _map.end()) {
+        if(_map[k-elem.first] != 0) {
             sum += elem.second * _map[k-elem.first];
             _map[k-elem.first] = 0;
         }
