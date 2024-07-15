6d33c880ac79f6d8aaa7f67e1c12d41f1ddc2a0d
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-16 00:57:34 +0900

    Title: 정수 명령어, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/정수 명령어/Integer-command.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int T, k, t;
+char com;
+set<int> oset;
+
+int main() {
+    
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> T;
+    for(int j = 0 ;j<T; j++){
+        cin >> k;
+
+        for(int i=0; i<k; i++){
+            cin >> com;
+            if(com == 'I'){
+                cin >> t;
+                oset.insert(t);
+            }
+            else if(com == 'D'){
+                cin >> t;
+                if(oset.empty()) continue;
+                if(t==1) oset.erase(*oset.rbegin());
+                else oset.erase(*oset.begin());
+            }
+        }
+
+        if(oset.empty()) cout << "EMPTY\n";
+        else cout << *oset.rbegin() << ' ' << *oset.begin() << '\n';
+    }
+    
+
+    return 0;
+}
