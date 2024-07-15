06a377c0439d266e76a42a62d996bfb092764bf1
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-15 17:18:03 +0900

    Title: 자리 바꾸기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240715/자리 바꾸기 2/changing-seats-2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+vector<unordered_set<int>> v;
+int arr[100001];
+int changes[100001][2];
+
+int n, k, a, b, t;
+
+int main() {
+    
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> n >> k;
+    v.resize(n);
+    for(int i=0; i<n; i++){
+        arr[i] = i;
+        v[i].insert(i);
+    }
+
+    for(int i=0; i<k; i++){
+        cin >> a >> b;
+        changes[i][0] = a; changes[i][1] = b;
+    }
+
+    
+    for(int t = 0; t<3; t++){
+    for(int i=0; i<k; i++){
+        a = changes[i][0]; b = changes[i][1];
+        
+        a--; b--;
+
+        t = arr[a];
+        arr[a] = arr[b];
+        arr[b] = t;
+
+        v[arr[a]].insert(a);
+        v[arr[b]].insert(b);
+    }
+    }
+
+    for(int i=0; i<n; i++){
+        cout << v[i].size() << '\n';
+    }
+
+
+    return 0;
+}
