6777cf463f6d52efb7485da2b09b35a357a7a1a8
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-15 13:11:59 +0900

    Title: 특별한 문자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240715/특별한 문자/special-character.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+string s;
+char c;
+int a[26];
+
+int main() {
+    
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    for(int i=0; i<26; i++) a[i] = 0;
+    
+    cin >> s;
+    for(int i=s.length()-1; i>=0; i--){
+        a[s[i]-'a']++;
+        if(a[s[i]-'a'] == 1) c = s[i];
+        else c = -1;   
+    }
+
+    if(c == -1) cout << "None";
+    else cout << c;
+    return 0;
+}
