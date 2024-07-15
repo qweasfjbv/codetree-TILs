3a8f343767efa391d29b1500344a2ab7b28f69f5
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-15 21:08:03 +0900

    Title: 초대장과 번호표, Time: 1095ms, Memory: 3MB, Status: Time Limit Exceed - CodeTree

--- 240715/초대장과 번호표/invitation-and-number-tag.cpp
+++ 240715/초대장과 번호표/invitation-and-number-tag.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 unordered_set<int> uset;
+unordered_set<int> checkset;
 vector<vector<int>> edges;
 
 int n, g, m, t;
@@ -12,12 +13,13 @@ void BFS(int idx){
 
     cin.tie(0);
     ios::sync_with_stdio(0);
-    
-    // OFFSET보다 작은 노드 -> 큰 노드와 연결되어있음
+
+    // n보다 작은 노드 -> 큰 노드와 연결되어있음
     // 그냥 BFS 돌림 (중복 가능해야함)
     if(idx < n) {
         uset.insert(idx);
         for(int i=0; i<edges[idx].size(); i++){
+            if(checkset.find(edges[idx][i]) != checkset.end()) continue;
             BFS(edges[idx][i]);
         }
 
@@ -28,6 +30,7 @@ void BFS(int idx){
     // 즉, 한 명 제외하고 전부 초대되어 있어야 다음 노드로 이동가능
     int cnt = 0;
     int nextIdx = -1;
+    checkset.insert(idx);
     for(int i=0; i<edges[idx].size(); i++){
         if(edges[idx][i] >= n) continue;
         if(uset.find(edges[idx][i]) != uset.end()) cnt++;
@@ -36,6 +39,8 @@ void BFS(int idx){
     }
 
     if(cnt == edges[idx].size()-1) BFS(nextIdx);
+
+    checkset.erase(idx);
     return;
 }
 
