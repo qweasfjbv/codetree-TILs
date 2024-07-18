019f720804779f967762826886947dc0c8ae9c34
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-18 20:59:40 +0900

    Title: 연속한 K개의 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/연속한 K개의 숫자/k-numbers-in-a-row.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n, k, b, t;
+int arr[100001];
+int sum[100001];
+
+int main() {
+    
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> n >> k >> b;
+
+    for(int i=0; i<=n; i++){
+        arr[t] = 0;
+    }
+    for(int i=0; i<b; i++){
+        cin >> t;
+        arr[t] = 1;
+    }
+
+    sum[0] = 0;
+    for(int i=1; i<=n; i++){
+        sum[i] = sum[i-1] + arr[i];
+    }
+
+    int _min = 100001;
+    for(int i=b; i<=n; i++){
+        _min = min(_min, sum[i]-sum[i-b]);
+    }
+
+    cout << _min;
+
+
+    return 0;
+}
