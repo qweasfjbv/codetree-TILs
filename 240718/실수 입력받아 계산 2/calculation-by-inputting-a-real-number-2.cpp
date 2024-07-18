83bc4b4ea44669519240a32ded4bfa54e5bd43c6
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-18 16:17:49 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cin >> a;
+    cout << fixed << setprecision(2) << a + 1.5;
+    return 0;
+}
