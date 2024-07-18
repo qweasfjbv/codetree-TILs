b7807e2d2cdb73d395f23b19f15dff617a65e8f0
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-18 19:54:37 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b, c;
+    cin >> a>> b >> c;
+
+    cout << fixed << setprecision(3) << a << '\n' << b << '\n' << c;
+    return 0;
+}
