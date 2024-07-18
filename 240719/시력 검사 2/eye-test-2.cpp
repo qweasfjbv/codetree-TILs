b4e7a45509cee987ca984bedd0ee4e8988d5b7cb
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-19 02:08:47 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+    if(d>=1.0) cout << "High";
+    else if(d>=0.5) cout << "Middle";
+    else cout <<"Low";
+    return 0;
+}
