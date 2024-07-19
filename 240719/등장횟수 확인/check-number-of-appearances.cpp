8c287f095e3622e24faa70dbb1a300c6aa1a44c6
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-19 21:49:19 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int cnt = 0;
+    for(int i=0; i<5; i++){
+        cin >> a;
+        if(a%2==0) cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
