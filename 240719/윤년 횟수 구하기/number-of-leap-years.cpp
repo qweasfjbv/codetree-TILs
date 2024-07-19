0a8f7120493f86493f4c5d4a8cf5205c70bfa824
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-19 21:48:39 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    int cnt =0;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        if(i%4==0){
+            if(i%100==0 && i%400!=0){
+            }
+            else 
+                cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
