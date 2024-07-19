6b0e264cba307cde1dd655492b15be63bc5aec34
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-20 02:04:44 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int cnt=0;
+    while(true){
+        cnt++;
+        if(n%2==0) n=n/2;
+        else n = n*3+1;
+
+        if(n==1) break;
+    }
+    cout << cnt;
+    return 0;
+}
