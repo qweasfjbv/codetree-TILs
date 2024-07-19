ba339025100950b1583d26af753317cbbc2e8f83
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-20 01:58:05 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/몫 출력하기/print-share.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt =0;
+    while(true){
+        cin >> n;
+        if(n%2==0){
+            cout << n/2 << '\n';
+            cnt++;
+            if(cnt==3) break;
+        }
+    }
+    
+    return 0;
+}
