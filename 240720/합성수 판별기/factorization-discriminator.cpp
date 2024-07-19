08a080d047b11821dd6a19e631895132da0665ea
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-20 02:17:10 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool once = false;
+    for(int i=2; i<n; i++){
+        if(n%i==0) {
+            once = true;break;
+        }
+    }
+    if(once) cout << "C";
+    else cout << "N";
+
+    return 0;
+}
