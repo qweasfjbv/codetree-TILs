89b1677e0a82f06e73f5c2b22d6d29268e944dea
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-20 02:47:39 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;cin >> n;
+    for(int i=1; i<=n; i++){
+        if(i%2==0){for(int j=0; j<i; j++)cout << "* ";
+        cout << '\n';
+        }
+        else {
+            cout << "*\n"; 
+        }
+    }
+    return 0;
+}
