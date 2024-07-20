413c8c7407c7c3f0764ae7287bbabea63289c245
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-21 02:07:47 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a, b, c, d;
+    cin >> n;
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        cin >> a>> b >> c >> d;
+        if((double)(a+b+c+d)/4>60){
+            cout << "pass\n";
+            cnt++;
+        }
+        else cout << "fail\n";
+    }
+    cout << cnt;
+    return 0;
+}
