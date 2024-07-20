7ebd15844b15acd654b391cc1ed124fcfa2a1eb1
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-21 02:06:14 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
+++ 240721/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
@@ -6,14 +6,15 @@ int main() {
     int cnt = 0;
     int a;
     int sum =0;
-
-    while(cnt != 10){
+    int count =0;
+    while(count != 10){
         cin >> a;
         if(a==0) break;
         if(a%2==0){
         cnt++;
         sum += a;
         }
+        count++;
     }
 
     cout << cnt << ' ' << sum;
