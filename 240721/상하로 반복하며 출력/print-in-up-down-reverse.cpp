0e65a4b1407b87039b836c5eb99de815d8a645ba
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-07-21 01:14:28 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n; cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(j%2==0){
+                cout << i+1;
+            }
+            else{
+                cout << n-i;
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
