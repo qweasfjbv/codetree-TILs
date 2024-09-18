7abd28d613dc4790d6229d185c723e01a3184503
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-09-18 15:13:39 +0900

    Title: 별 그리기, Time: 249ms, Memory: 14MB, Status: Passed - CodeTree

--- 240918/별 그리기/star-drawing.java
+++ 240918/별 그리기/star-drawing.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i=0; i<n; i++){
-            for (int j=n-i-1; j>0; j--){
+            for (int j=0; j<n-i-1; j++){
                 System.out.print(" ");   
             }
             for (int j=0; j<2*i+1; j++){
@@ -17,7 +17,7 @@ public class Main {
             for (int j=0; j<=i; j++){
                 System.out.print(" ");
             }
-            for (int j=-2*i+3; j>0; j--){
+            for (int j=0; j<2*(n-i-1)-1; j++){
                 System.out.print("*");
             }
             System.out.println();
