5d25157dba3af72239bd6f3c403df8ef23ea8123
qweasfjbv <68148581+qweasfjbv@users.noreply.github.com>
2024-10-29 12:44:15 +0900

    Title: 씨 오 더블유, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241029/씨 오 더블유/c-o-w.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int arr[3] = {0,0,0};
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n; cin>>n;
+    string s;
+    cin >> s;
+    for(int i=0; i<s.size(); i++){
+        switch(s[i]){
+            case 'C':
+            arr[0]++;
+            break;
+            case 'O':
+            arr[1] += arr[0];
+            break;
+            case 'W':
+            arr[2] += arr[1];
+            break;
+        }
+    }
+
+    cout << arr[2];
+    return 0;
+}
